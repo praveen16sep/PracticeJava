1259eedb12c2eb604bd7174983bf51de428209b8
praveen16sep <68071640+praveen16sep@users.noreply.github.com>
2025-06-13 08:47:36 +0530

    Add files via upload

---
+++ CountOfEachWordAsendOrder.java
@@ -0,0 +1,38 @@
+package com.java8code;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class CountOfEachWordAsendOrder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> items =
+                Arrays.asList("apple", "apple", "banana",
+                        "apple", "orange", "banana", "papaya");
+
+        Map<String, Long> result =
+                items.stream().collect(
+                        Collectors.groupingBy(
+                                Function.identity(), Collectors.counting()
+                        )
+                );
+        
+        System.out.println(result);
+        
+        Map<String, Long> finalMap = new LinkedHashMap<>();
+
+        //Sort a map and add to finalMap
+        result.entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue()
+                        .reversed()).forEachOrdered(e -> finalMap.put(e.getKey(), e.getValue()));
+
+        System.out.println(finalMap);
+
+	}
+
+}
